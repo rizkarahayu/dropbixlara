6e007e316016325b0df1035c8e6a4bf094f481c6
rizkarahayyu <rizkarahayu42@gmail.com>
2019-04-05 14:24:31 +0700

    home & partials view

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-inverse">
+         <div class="container-fluid">
+            <div class="navbar-header">
+               <a class="navbar-brand" href="#">DropBix</a>
+            </div>
+            <ul class="nav navbar-nav">
+               <li class="active"><a href="#">Home</a></li>
+               <li class="active"><a href="#">Upload File</a></li>
+               <li class="active"><a href="#">Dowload File</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+               <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+               <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+         </div>
+</nav>
+<br>
+<br>
---
+++ resources/views/template.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html class= "no-js" lang="en">
+
+@include('partials.header')
+@include('partials.nav')
+@yield('content')
+@include('partials.footer')
